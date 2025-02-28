3a0342658aa682b791f8c8962aa4bfc3b869694c
qus0in <qus0in@gmail.com>
2025-02-28 11:37:58 +0900

    first commit

---
+++ src/Solution01.java
@@ -0,0 +1,20 @@
+import llm.*;
+
+public class Solution01 {
+    public static void main(String[] args) {
+        // LLM 슬랙봇으로 복습하자
+        // 왜 복습인가? - 1. 어제 분명 수업한 내용이 있다 2. 우리는 LLM과 슬랙봇을 하였습니다
+        LLM llm = new LLM();
+        Slack slack = new Slack();
+    }
+}
+
+
+
+
+class Slack extends WebClient {
+
+}
+
+
+
---
+++ src/web_client/WebClient.java
@@ -0,0 +1,9 @@
+package web_client;
+
+public abstract class WebClient implements IWebClient {
+
+}
+
+// default -> 같은 패키지 내부에선 자유롭게 사용 가능
+interface IWebClient {
+}
